e6228111010200fba786b98275b704255703685e
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-10-03 16:35:20 +0300

    Update Home.tsx

--- youtube-react-clone/src/page/Home.tsx
+++ youtube-react-clone/src/page/Home.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../component/Navbar/Navbar";
 import Sidebar from "../component/Sidebar/Sidebar";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { getHomePageVideos } from "../redux/reducers/getHomePageVideos";
 
 const Home = () => {
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+
+  useEffect(() => {
+    dispatch(getHomePageVideos(false));
+  }, [dispatch]);
+
   return (
     <div className="max-h-screen overflow-hidden">
       <div style={{ height: "7.5vh" }}>
