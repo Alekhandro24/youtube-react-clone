6f52dfbe194c91ef860340ff523d185900cedc24
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-10-05 15:03:52 +0300

    Update Home.tsx

--- youtube-react-clone/src/page/Home.tsx
+++ youtube-react-clone/src/page/Home.tsx
@@ -3,6 +3,10 @@ import Navbar from "../component/Navbar/Navbar";
 import Sidebar from "../component/Sidebar/Sidebar";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { getHomePageVideos } from "../redux/reducers/getHomePageVideos";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Spinner from "../component/Spinner/Spinner";
+import { HomePageVideos } from "../Types";
+import Card from "../component/Card/Card";
 
 const Home = () => {
   const dispatch = useAppDispatch();
@@ -19,6 +23,23 @@ const Home = () => {
       </div>
       <div className="flex" style={{ height: "92.5vh" }}>
         <Sidebar />
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getHomePageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={650}
+          >
+            <div className="grid gap-y-14 gap-x-8 grid-cols-4 p-4">
+              {videos.map((item: HomePageVideos) => {
+                return <Card data={item} key={item.videoId} />;
+              })}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
       </div>
     </div>
   );
