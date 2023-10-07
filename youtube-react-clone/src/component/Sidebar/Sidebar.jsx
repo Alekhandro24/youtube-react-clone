88f89732d9f3450e71a4cc6b0f5a0a65490ec11c
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-10-07 18:06:01 +0300

    Update Sidebar.jsx

--- youtube-react-clone/src/component/Sidebar/Sidebar.jsx
+++ youtube-react-clone/src/component/Sidebar/Sidebar.jsx
@@ -48,12 +48,12 @@ const Sidebar = () => {
       name: "History",
     },
     {
-      icon: <MdOutlineWatchLater className="text-xl" />,
-      name: "Watch Later",
-    },
-    {
       icon: <MdOutlineSmartDisplay className="text-xl" />,
       name: "Your Videos",
+    },
+    {
+      icon: <MdOutlineWatchLater className="text-xl" />,
+      name: "Watch Later",
     },
     {
       icon: <MdThumbUpOffAlt className="text-xl" />,
@@ -86,12 +86,12 @@ const Sidebar = () => {
       name: "Settings",
     },
     {
-      icon: <MdOutlineHelpOutline className="text-xl" />,
-      name: "Hepl",
+      icon: <MdOutlinedFlag className="text-xl" />,
+      name: "Report history",
     },
     {
-      icon: <MdOutlinedFlag className="text-xl" />,
-      name: "Report History",
+      icon: <MdOutlineHelpOutline className="text-xl" />,
+      name: "Help",
     },
     {
       icon: <MdOutlineFeedback className="text-xl" />,
@@ -121,49 +121,57 @@ const Sidebar = () => {
   return (
     <div className="w-2/12 bg-[#212121] pr-5 overflow-auto pb-8 sidebar">
       <ul className="flex flex-col border-b-2 border-gray-700">
-        {mainLinks.map(({ icon, name }) => (
-          <li
-            key={name}
-            className={`pl-6 py-3 hover:bg-zinc-600 ${
-              name === "Home" ? "bg-slate-600" : ""
-            }`}
-          >
-            <a href="#" className="flex items-center gap-5">
-              {icon}
-              <span className="text-sm tracking-wider">{name}</span>
-            </a>
-          </li>
-        ))}
+        {mainLinks.map(({ icon, name }) => {
+          return (
+            <li
+              key={name}
+              className={`pl-6 py-3 hover:bg-zinc-600 ${
+                name === "Home" ? "bg-slate-600" : ""
+              }`}
+            >
+              <a href="#" className="flex items-center gap-5">
+                {icon}
+                <span className="text-sm tracking-wider">{name}</span>
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <ul className="flex flex-col border-b-2 border-gray-700">
-        {secondaryLinks.map(({ icon, name }) => (
-          <li key={name} className="pl-6 py-3 hover:bg-zinc-600 ">
-            <a href="#" className="flex items-center gap-5">
-              {icon}
-              <span className="text-sm tracking-wider">{name}</span>
-            </a>
-          </li>
-        ))}
+        {secondaryLinks.map(({ icon, name }) => {
+          return (
+            <li key={name} className={`pl-6 py-3 hover:bg-zinc-600 `}>
+              <a href="#" className="flex items-center gap-5">
+                {icon}
+                <span className="text-sm tracking-wider">{name}</span>
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <ul className="flex flex-col border-b-2 border-gray-700">
-        {subscriptionLinks.map(({ icon, name }) => (
-          <li key={name} className="pl-6 py-3 hover:bg-zinc-600 ">
-            <a href="#" className="flex items-center gap-5">
-              {icon}
-              <span className="text-sm tracking-wider">{name}</span>
-            </a>
-          </li>
-        ))}
+        {subscriptionLinks.map(({ icon, name }) => {
+          return (
+            <li key={name} className={`pl-6 py-3 hover:bg-zinc-600 `}>
+              <a href="#" className="flex items-center gap-5">
+                {icon}
+                <span className="text-sm tracking-wider">{name}</span>
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <ul className="flex flex-col border-b-2 border-gray-700">
-        {helpLinks.map(({ icon, name }) => (
-          <li key={name} className="pl-6 py-3 hover:bg-zinc-600 ">
-            <a href="#" className="flex items-center gap-5">
-              {icon}
-              <span className="text-sm tracking-wider">{name}</span>
-            </a>
-          </li>
-        ))}
+        {helpLinks.map(({ icon, name }) => {
+          return (
+            <li key={name} className={`pl-6 py-3 hover:bg-zinc-600 `}>
+              <a href="#" className="flex items-center gap-5">
+                {icon}
+                <span className="text-sm tracking-wider">{name}</span>
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <ul className="flex gap-2 flex-wrap text-sm p-4 text-zinc-400">
         {textLinks[0].map((name) => {
