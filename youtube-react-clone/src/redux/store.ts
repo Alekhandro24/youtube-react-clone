1f7bc53c6cc3d15a019590a189dcd893b477bd9c
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-10-07 18:08:08 +0300

    Update store.ts

--- youtube-react-clone/src/redux/store.ts
+++ youtube-react-clone/src/redux/store.ts
@@ -1,6 +1,9 @@
 import { InitialState } from "../Types";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
 import { getHomePageVideos } from "./reducers/getHomePageVideos";
+import { getSearchPageVideos } from "./reducers/getSearchPageVideos";
+import { getRecommendedVideos } from "./reducers/getRecommendedVideos";
+import { getVideoDetails } from "./reducers/getVideoDetails";
 
 const initialState: InitialState = {
   videos: [],
@@ -14,12 +17,34 @@ const initialState: InitialState = {
 const YoutubeSlice = createSlice({
   name: "youtubeApp",
   initialState,
-  reducers: {},
+  reducers: {
+    clearVideos: (state) => {
+      state.videos = [];
+      state.nextPageToken = null;
+    },
+    changeSearchTerm: (state, action: PayloadAction<string>) => {
+      state.searchTerm = action.payload;
+    },
+    clearSearchTerm: (state) => {
+      state.searchTerm = "";
+    },
+  },
   extraReducers: (builder) => {
-    builder.addCase(getHomePageVideos.fulfilled, (state, action) => {
-      state.videos = action.payload.parsedData;
-      state.nextPageToken = action.payload.nextPageToken;
-    });
+    builder
+      .addCase(getHomePageVideos.fulfilled, (state, action) => {
+        state.videos = action.payload.parsedData;
+        state.nextPageToken = action.payload.nextPageToken;
+      })
+      .addCase(getSearchPageVideos.fulfilled, (state, action) => {
+        state.videos = action.payload.parsedData;
+        state.nextPageToken = action.payload.nextPageToken;
+      })
+      .addCase(getVideoDetails.fulfilled, (state, action) => {
+        state.currentPlaying = action.payload;
+      })
+      .addCase(getRecommendedVideos.fulfilled, (state, action) => {
+        state.recommendedVideos = action.payload.parsedData;
+      });
   },
 });
 
@@ -28,6 +53,9 @@ export const store = configureStore({
     youtubeApp: YoutubeSlice.reducer,
   },
 });
+
+export const { clearVideos, changeSearchTerm, clearSearchTerm } =
+  YoutubeSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
