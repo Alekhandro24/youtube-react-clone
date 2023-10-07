08925b45c5304739f13ce52ddd7e61d54c1dbff5
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-10-07 18:05:08 +0300

    Update Navbar.tsx

--- youtube-react-clone/src/component/Navbar/Navbar.tsx
+++ youtube-react-clone/src/component/Navbar/Navbar.tsx
@@ -4,13 +4,35 @@ import { TiMicrophone } from "react-icons/ti";
 import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoAppsSharp } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import {
+  changeSearchTerm,
+  clearSearchTerm,
+  clearVideos,
+} from "../../redux/store";
+import { getSearchPageVideos } from "../../redux/reducers/getSearchPageVideos";
 
 const Navbar = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+  const handleSearch = () => {
+    if (location.pathname !== "/search") navigate("/search");
+    else {
+      dispatch(clearVideos());
+      dispatch(getSearchPageVideos(false));
+    }
+  };
+
   return (
-    <div className="flex justify-between items-center px-14 h-14 bg-[#212121] opacity-95 sticky top-0 z-50">
+    <div className="flex justify-between items-center  px-14 h-14 bg-[#212121] opacity-95 sticky top-0 z-50">
       <div className="flex gap-8 items-center text-2xl">
-        <GiHamburgerMenu />
+        <div>
+          <GiHamburgerMenu />
+        </div>
         <Link to="/">
           <div className="flex gap-1 items-center justify-center">
             <BsYoutube className="text-3xl text-red-600" />
@@ -19,7 +41,12 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="flex items-center justify-center gap-5">
-        <form action="">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSearch();
+          }}
+        >
           <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0">
             <div className="flex gap-4 items-center pr-5">
               <div>
@@ -28,9 +55,16 @@ const Navbar = () => {
               <input
                 type="text"
                 className="w-96 bg-zinc-900 focus:outline-none border-none"
+                value={searchTerm}
+                onChange={(e) => dispatch(changeSearchTerm(e.target.value))}
               />
 
-              <AiOutlineClose className="text-xl cursor-pointer" />
+              <AiOutlineClose
+                className={`text-xl cursor-pointer ${
+                  !searchTerm ? "invisible" : "visible"
+                }`}
+                onClick={() => dispatch(clearSearchTerm())}
+              />
             </div>
             <button className="h-10 w-16 flex items-center justify-center bg-zinc-800">
               <AiOutlineSearch className="text-xl" />
@@ -52,8 +86,8 @@ const Navbar = () => {
         </div>
         <img
           src="https://www.svgrepo.com/show/13671/youtube.svg"
-          alt="logo"
           className="w-9 h-9 rounded-full"
+          alt="logo"
         />
       </div>
     </div>
