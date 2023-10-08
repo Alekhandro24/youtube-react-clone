629147809e185a0a4cc3751d0c9fb71b85455f6c
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-10-08 11:03:19 +0300

    Update Navbar.tsx

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
-import { TiMicrophone } from "react-icons/ti";
-import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { IoAppsSharp } from "react-icons/io5";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import React from 'react';
+import { AiOutlineSearch, AiOutlineClose } from 'react-icons/ai';
+import { TiMicrophone } from 'react-icons/ti';
+import { BsYoutube, BsCameraVideo, BsBell } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoAppsSharp } from 'react-icons/io5';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import {
   changeSearchTerm,
   clearSearchTerm,
   clearVideos,
-} from "../../redux/store";
-import { getSearchPageVideos } from "../../redux/reducers/getSearchPageVideos";
+} from '../../redux/store';
+import { getSearchPageVideos } from '../../redux/reducers/getSearchPageVideos';
 
 const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+  const searchTerm = useAppSelector(state => state.youtubeApp.searchTerm);
   const handleSearch = () => {
-    if (location.pathname !== "/search") navigate("/search");
+    if (location.pathname !== '/search') navigate('/search');
     else {
       dispatch(clearVideos());
       dispatch(getSearchPageVideos(false));
@@ -42,7 +42,7 @@ const Navbar = () => {
       </div>
       <div className="flex items-center justify-center gap-5">
         <form
-          onSubmit={(e) => {
+          onSubmit={e => {
             e.preventDefault();
             handleSearch();
           }}
@@ -56,12 +56,12 @@ const Navbar = () => {
                 type="text"
                 className="w-96 bg-zinc-900 focus:outline-none border-none"
                 value={searchTerm}
-                onChange={(e) => dispatch(changeSearchTerm(e.target.value))}
+                onChange={e => dispatch(changeSearchTerm(e.target.value))}
               />
 
               <AiOutlineClose
                 className={`text-xl cursor-pointer ${
-                  !searchTerm ? "invisible" : "visible"
+                  !searchTerm ? 'invisible' : 'visible'
                 }`}
                 onClick={() => dispatch(clearSearchTerm())}
               />
