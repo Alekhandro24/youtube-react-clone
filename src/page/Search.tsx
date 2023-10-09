59056ce2db82de14700eaeb1e6c58289b830e20c
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-10-09 17:26:53 +0300

    Update Search.tsx

--- src/page/Search.tsx
+++ src/page/Search.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect } from "react";
-import Navbar from "../component/Navbar/Navbar";
-import Sidebar from "../component/Sidebar/Sidebar";
-import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import InfiniteScroll from "react-infinite-scroll-component";
-import Spinner from "../component/Spinner/Spinner";
-import { HomePageVideos } from "../Types";
-import { clearVideos } from "../redux/store";
-import { useNavigate } from "react-router-dom";
-import { getSearchPageVideos } from "../redux/reducers/getSearchPageVideos";
-import SearchCard from "../component/SearchCard/SearchCard";
+import React, { useEffect } from 'react';
+import Navbar from '../component/Navbar/Navbar';
+import Sidebar from '../component/Sidebar/Sidebar';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Spinner from '../component/Spinner/Spinner';
+import { HomePageVideos } from '../Types';
+import { clearVideos } from '../redux/store';
+import { useNavigate } from 'react-router-dom';
+import { getSearchPageVideos } from '../redux/reducers/getSearchPageVideos';
+import SearchCard from '../component/SearchCard/SearchCard';
 
 const Search = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const videos = useAppSelector((state) => state.youtubeApp.videos);
-  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+  const videos = useAppSelector(state => state.youtubeApp.videos);
+  const searchTerm = useAppSelector(state => state.youtubeApp.searchTerm);
 
   useEffect(() => {
     dispatch(clearVideos());
-    if (searchTerm === "") navigate("/");
+    if (searchTerm === '') navigate('/');
     else {
       dispatch(getSearchPageVideos(false));
     }
@@ -26,10 +26,10 @@ const Search = () => {
 
   return (
     <div className="max-h-screen overflow-hidden">
-      <div style={{ height: "7.5vh" }}>
+      <div style={{ height: '7.5vh' }}>
         <Navbar />
       </div>
-      <div className="flex" style={{ height: "92.5vh" }}>
+      <div className="flex" style={{ height: '92.5vh' }}>
         <Sidebar />
         {videos.length ? (
           <div className="py-8 pl-8 flex flex-col gap-5 w-full">
